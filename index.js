bdb648f848bcd1a64c24d551ef11ced1e1e0073e
CD Cabrera <cabrera.code@gmail.com>
2017-03-15 02:18:49 -0400

    DEV Ecma Update
    Initial conversion to Ecma 6, 2015 or whatever it’s been rebranded as

--- index.js
+++ index.js
@@ -1,96 +1,100 @@
 
-    var fs          = require('fs'),
-        drafter     = require('drafter'),
-        express     = require('express'),
-        walker      = require('./lib/walker'),
-        sslSupport  = require('./lib/ssl-support'),
-        corsSupport = require('./lib/cors-support');
+    const   fs          = require('fs'),
+            drafter     = require('drafter'),
+            express     = require('express'),
+            walker      = require('./lib/walker'),
+            sslSupport  = require('./lib/ssl-support'),
+            corsSupport = require('./lib/cors-support');
+
+
+    class apiMock {
+
+        constructor(settings) {
+
+            this.settings = Object.assign({
+
+                blueprintPath: null,
+                options: {
+                    port: 8100,
+                    'ssl-enable': null,
+                    'ssl-port': null,
+                    'ssl-cert': null,
+                    'ssl-key': null,
+                    'cors-disable': null
+                }
+
+            }, settings);
+        }
+
+        run() {
+
+            let settings    = this.settings,
+                data        = '';
+
+
+            try {
+
+                data = fs.readFileSync(settings.blueprintPath, 'utf8');
+
+            } catch(e){
+
+                throw e;
+            }
+
+
+            drafter.parse(data, {type: "ast"}, function(error, result) {
+
+                if (error !== null) {
+
+                    throw error;
+                }
+
+
+                let app = express();
+
+
+                if (settings.options['ssl-enable']) {
+
+                    sslSupport(app, {
+
+                        port: settings.options['ssl-port'],
+                        host: settings.options['ssl-host'],
+                        cert: settings.options['ssl-cert'],
+                        key: settings.options['ssl-key']
+                    });
+                }
+
+
+                if (!settings.options['cors-disable']) {
+
+                    corsSupport(app);
+                }
+
+
+                try {
+
+                    walker(app, result.ast['resourceGroups']);
+
+                } catch (e) {
+
+                    throw error;
+                }
+
+
+                try {
+
+                    return app.listen(settings.options.port);
+
+                } catch (e) { }
+            });
+        }
+    }
 
 
     module.exports = apiMock;
 
+    /*const test = new apiMock({
+        blueprintPath: './samples/test.apib'
+    });
 
-    //apiMock({
-    //    blueprintPath: './samples/test.apib'
-    //});
-
-    /**
-     *
-     * @param settings
-     */
-    function apiMock(settings) {
-
-        settings = Object.assign({
-
-            blueprintPath: null,
-            options: {
-                port: 3000,
-                'ssl-enable': null,
-                'ssl-port': null,
-                'ssl-cert': null,
-                'ssl-key': null,
-                'cors-disable': null
-            }
-
-        }, settings);
-
-
-        var data = '';
-
-
-        try {
-
-            data = fs.readFileSync(settings.blueprintPath, 'utf8');
-
-        } catch(e){
-
-            throw e;
-        }
-
-
-        drafter.parse(data, {type: "ast"}, function(error, result) {
-
-            if (error !== null) {
-
-                throw error;
-            }
-
-
-            var app = express();
-
-
-            if (settings.options['ssl-enable']) {
-
-                sslSupport(app, {
-
-                    port: settings.options['ssl-port'],
-                    host: settings.options['ssl-host'],
-                    cert: settings.options['ssl-cert'],
-                    key: settings.options['ssl-key']
-                 });
-            }
-
-
-            if (!settings.options['cors-disable']) {
-
-                corsSupport(app);
-            }
-
-
-            try {
-
-                walker(app, result.ast['resourceGroups']);
-
-            } catch (e) {
-
-                throw error;
-            }
-
-
-            try {
-
-                return app.listen(settings.options.port);
-
-            } catch (e) {}
-        });
-    }
+    test.run();*/
